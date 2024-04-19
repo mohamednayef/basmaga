dcdff36c5b2bfe95f8272fc5df2c4e3958bf832e
mohamednayef <mohamednayef2002@gmail.com>
2024-04-19 09:37:47 +0200

    a

---
+++ examd/exams_nayef.js
@@ -0,0 +1,582 @@
+let graphics_2021_button = document.querySelectorAll('.graphics_2021_button');
+let graphics_2021_question = document.querySelectorAll('.graphics_2021_question');
+let graphics_2021_next = document.getElementById('graphics_2021_next');
+let graphics_2021_previous = document.getElementById('graphics_2021_previous');
+
+// ******************************************
+function isHidden(element) {
+    if (element) {
+        var displayStyle = window.getComputedStyle(element).getPropertyValue('display');
+        return displayStyle === 'none';
+    }
+    return false; // Return false if the element doesn't exist
+}
+
+
+function next(button, elements) {
+    button.onclick = function() {
+        for (var i = 0; i < elements.length - 1; i++) {
+            if (!isHidden(elements[i])) {
+                elements[i].style.display = 'none';
+                elements[i + 1].style.display = 'block';
+                break; // Exit loop after the first visible element is found and handled
+            }
+        }
+    }
+}
+
+next(graphics_2021_next, graphics_2021_question);
+
+function previous(button, elements) {
+    button.onclick = function() {
+        for (var i = 1; i <= elements.length - 1; i++) {
+            if (!isHidden(elements[i])) {
+                elements[i].style.display = 'none';
+                elements[i - 1].style.display = 'block';
+                break; // Exit loop after the first visible element is found and handled
+            }
+        }
+    }
+}
+
+previous(graphics_2021_previous, graphics_2021_question);
+// ******************************************
+
+function show1(button, question) {
+    button.onclick = function() {
+        for(var i=0; i<6; i++) {
+            graphics_2021_question[i].style.display = 'none';
+        }
+        question.style.display = 'block';
+    }
+}
+
+// function isHidden(elementId) {
+//     // var element = document.getElementById(elementId);
+//     // var element = document.querySelector(elementClassName);
+//     if (elementId) {
+//         var displayStyle = window.getComputedStyle(elementId).getPropertyValue('display');
+//         return displayStyle === 'none';
+//     }
+//     return false; // Return false if the element doesn't exist
+// }
+
+
+
+// Example usage
+// var isElementHidden = isHidden('yourElementId');
+// console.log('Is element hidden?', isElementHidden);
+
+
+
+// function next(button, elements) {
+//     button.onclick = function() {
+//         // console.log(elements);
+//         // console.log(elements[0].id);
+//         for(var i=0; i<6; i++) {
+//             if(isHidden(elements[i].id) === false) {
+//                 elements[i].style.display = 'none';
+//                 elements[i+1].style.display = 'block';
+//             }
+//         }
+//     }
+// }
+
+// next(graphics_2021_next, graphics_2021_question);
+
+for(let i=0; i<6; i++) {
+    show1(graphics_2021_button[i], graphics_2021_question[i]);
+}
+
+// function show(id_of_question) {
+//     document.getElementById(id_of_question).style.display = 'block';
+// }
+
+// show("graphics_2021_question5");
+// let q1=document.getElementById('q1'); 
+// let q2=document.getElementById('q2');
+// let q3=document.getElementById('q3');
+// let q4=document.getElementById('q4');
+// let q5=document.getElementById('q5');
+// let q6=document.getElementById('q6');
+// let q7=document.getElementById('q7');
+// let q8=document.getElementById('q8');
+// let q9=document.getElementById('q9');
+// let q10=document.getElementById('q10');
+// let q11=document.getElementById('q11');
+// let q12=document.getElementById('q12');
+// let q13=document.getElementById('q13');
+// let q14=document.getElementById('q14');
+// let q15=document.getElementById('q15');
+// let q16=document.getElementById('q16');
+// let q17=document.getElementById('q17');
+// let q18=document.getElementById('q18');
+// let q19=document.getElementById('q19');
+// let q20=document.getElementById('q20');
+
+
+
+// let num1=document.getElementById('num1');
+// let num2=document.getElementById('num2');
+// let num3=document.getElementById('num3');
+// let num4=document.getElementById('num4');
+// let num5=document.getElementById('num5');
+// let num6=document.getElementById('num6');
+// let num7=document.getElementById('num7');
+// let num8=document.getElementById('num8');
+// let num9=document.getElementById('num9');
+// let num10=document.getElementById('num10');
+// let num11=document.getElementById('num11');
+// let num12=document.getElementById('num12');
+// let num13=document.getElementById('num13');
+// let num14=document.getElementById('num14');
+// let num15=document.getElementById('num15');
+// let num16=document.getElementById('num16');
+// let num17=document.getElementById('num17');
+// let num18=document.getElementById('num18');
+// let num19=document.getElementById('num19');
+// let num20=document.getElementById('num20');
+
+
+
+
+// num1.onclick=function(){
+//     q1.style.display='flex';
+//     q2.style.display='none';
+//     q3.style.display='none';
+//     q4.style.display='none';
+//     q5.style.display='none';
+//     q6.style.display='none';
+//     q7.style.display='none';
+//     q8.style.display='none';
+//     q9.style.display='none';
+//     q10.style.display='none';
+//     q11.style.display='none';
+//     q12.style.display='none';
+//     q13.style.display='none';
+//     q14.style.display='none';
+//     q15.style.display='none';
+//     q16.style.display='none';
+//     q17.style.display='none';
+//     q18.style.display='none';
+//     q19.style.display='none';
+//     q20.style.display='none';
+// }
+// num2.onclick=function(){
+//     q1.style.display='none';
+//     q2.style.display='flex';
+//     q3.style.display='none';
+//     q4.style.display='none';
+//     q5.style.display='none';
+//     q6.style.display='none';
+//     q7.style.display='none';
+//     q8.style.display='none';
+//     q9.style.display='none';
+//     q10.style.display='none';
+//     q11.style.display='none';
+//     q12.style.display='none';
+//     q13.style.display='none';
+//     q14.style.display='none';
+//     q15.style.display='none';
+//     q16.style.display='none';
+//     q17.style.display='none';
+//     q18.style.display='none';
+//     q19.style.display='none';
+//     q20.style.display='none';
+// }
+// num3.onclick=function(){
+//     q1.style.display='none';
+//     q2.style.display='none';
+//     q3.style.display='flex';
+//     q4.style.display='none';
+//     q5.style.display='none';
+//     q6.style.display='none';
+//     q7.style.display='none';
+//     q8.style.display='none';
+//     q9.style.display='none';
+//     q10.style.display='none';
+//     q11.style.display='none';
+//     q12.style.display='none';
+//     q13.style.display='none';
+//     q14.style.display='none';
+//     q15.style.display='none';
+//     q16.style.display='none';
+//     q17.style.display='none';
+//     q18.style.display='none';
+//     q19.style.display='none';
+//     q20.style.display='none';
+// }
+// num4.onclick=function(){
+//     q1.style.display='none';
+//     q2.style.display='none';
+//     q3.style.display='none';
+//     q4.style.display='flex';
+//     q5.style.display='none';
+//     q6.style.display='none';
+//     q7.style.display='none';
+//     q8.style.display='none';
+//     q9.style.display='none';
+//     q10.style.display='none';
+//     q11.style.display='none';
+//     q12.style.display='none';
+//     q13.style.display='none';
+//     q14.style.display='none';
+//     q15.style.display='none';
+//     q16.style.display='none';
+//     q17.style.display='none';
+//     q18.style.display='none';
+//     q19.style.display='none';
+//     q20.style.display='none';
+// }
+// num5.onclick=function(){
+//     q1.style.display='none';
+//     q2.style.display='none';
+//     q3.style.display='none';
+//     q4.style.display='none';
+//     q5.style.display='flex';
+//     q6.style.display='none';
+//     q7.style.display='none';
+//     q8.style.display='none';
+//     q9.style.display='none';
+//     q10.style.display='none';
+//     q11.style.display='none';
+//     q12.style.display='none';
+//     q13.style.display='none';
+//     q14.style.display='none';
+//     q15.style.display='none';
+//     q16.style.display='none';
+//     q17.style.display='none';
+//     q18.style.display='none';
+//     q19.style.display='none';
+//     q20.style.display='none';
+// }
+// num6.onclick=function(){
+//     q1.style.display='none';
+//     q2.style.display='none';
+//     q3.style.display='none';
+//     q4.style.display='none';
+//     q5.style.display='none';
+//     q6.style.display='flex';
+//     q7.style.display='none';
+//     q8.style.display='none';
+//     q9.style.display='none';
+//     q10.style.display='none';
+//     q11.style.display='none';
+//     q12.style.display='none';
+//     q13.style.display='none';
+//     q14.style.display='none';
+//     q15.style.display='none';
+//     q16.style.display='none';
+//     q17.style.display='none';
+//     q18.style.display='none';
+//     q19.style.display='none';
+//     q20.style.display='none';
+// }
+// num7.onclick=function(){
+//     q1.style.display='none';
+//     q2.style.display='none';
+//     q3.style.display='none';
+//     q4.style.display='none';
+//     q5.style.display='none';
+//     q6.style.display='none';
+//     q7.style.display='flex';
+//     q8.style.display='none';
+//     q9.style.display='none';
+//     q10.style.display='none';
+//     q11.style.display='none';
+//     q12.style.display='none';
+//     q13.style.display='none';
+//     q14.style.display='none';
+//     q15.style.display='none';
+//     q16.style.display='none';
+//     q17.style.display='none';
+//     q18.style.display='none';
+//     q19.style.display='none';
+//     q20.style.display='none';
+// }
+// num8.onclick=function(){
+//     q1.style.display='none';
+//     q2.style.display='none';
+//     q3.style.display='none';
+//     q4.style.display='none';
+//     q5.style.display='none';
+//     q6.style.display='none';
+//     q7.style.display='none';
+//     q8.style.display='flex';
+//     q9.style.display='none';
+//     q10.style.display='none';
+//     q11.style.display='none';
+//     q12.style.display='none';
+//     q13.style.display='none';
+//     q14.style.display='none';
+//     q15.style.display='none';
+//     q16.style.display='none';
+//     q17.style.display='none';
+//     q18.style.display='none';
+//     q19.style.display='none';
+//     q20.style.display='none';
+// }
+// num9.onclick=function(){
+//     q1.style.display='none';
+//     q2.style.display='none';
+//     q3.style.display='none';
+//     q4.style.display='none';
+//     q5.style.display='none';
+//     q6.style.display='none';
+//     q7.style.display='none';
+//     q8.style.display='none';
+//     q9.style.display='flex';
+//     q10.style.display='none';
+//     q11.style.display='none';
+//     q12.style.display='none';
+//     q13.style.display='none';
+//     q14.style.display='none';
+//     q15.style.display='none';
+//     q16.style.display='none';
+//     q17.style.display='none';
+//     q18.style.display='none';
+//     q19.style.display='none';
+//     q20.style.display='none';
+// }
+// num10.onclick=function(){
+//     q1.style.display='none';
+//     q2.style.display='none';
+//     q3.style.display='none';
+//     q4.style.display='none';
+//     q5.style.display='none';
+//     q6.style.display='none';
+//     q7.style.display='none';
+//     q8.style.display='none';
+//     q9.style.display='none';
+//     q10.style.display='flex';
+//     q11.style.display='none';
+//     q12.style.display='none';
+//     q13.style.display='none';
+//     q14.style.display='none';
+//     q15.style.display='none';
+//     q16.style.display='none';
+//     q17.style.display='none';
+//     q18.style.display='none';
+//     q19.style.display='none';
+//     q20.style.display='none';
+// }
+// num11.onclick=function(){
+//     q1.style.display='none';
+//     q2.style.display='none';
+//     q3.style.display='none';
+//     q4.style.display='none';
+//     q5.style.display='none';
+//     q6.style.display='none';
+//     q7.style.display='none';
+//     q8.style.display='none';
+//     q9.style.display='none';
+//     q10.style.display='none';
+//     q11.style.display='flex';
+//     q12.style.display='none';
+//     q13.style.display='none';
+//     q14.style.display='none';
+//     q15.style.display='none';
+//     q16.style.display='none';
+//     q17.style.display='none';
+//     q18.style.display='none';
+//     q19.style.display='none';
+//     q20.style.display='none';
+// }
+// num12.onclick=function(){
+//     q1.style.display='none';
+//     q2.style.display='none';
+//     q3.style.display='none';
+//     q4.style.display='none';
+//     q5.style.display='none';
+//     q6.style.display='none';
+//     q7.style.display='none';
+//     q8.style.display='none';
+//     q9.style.display='none';
+//     q10.style.display='none';
+//     q11.style.display='none';
+//     q12.style.display='flex';
+//     q13.style.display='none';
+//     q14.style.display='none';
+//     q15.style.display='none';
+//     q16.style.display='none';
+//     q17.style.display='none';
+//     q18.style.display='none';
+//     q19.style.display='none';
+//     q20.style.display='none';
+// }
+// num13.onclick=function(){
+//     q1.style.display='none';
+//     q2.style.display='none';
+//     q3.style.display='none';
+//     q4.style.display='none';
+//     q5.style.display='none';
+//     q6.style.display='none';
+//     q7.style.display='none';
+//     q8.style.display='none';
+//     q9.style.display='none';
+//     q10.style.display='none';
+//     q11.style.display='none';
+//     q12.style.display='none';
+//     q13.style.display='flex';
+//     q14.style.display='none';
+//     q15.style.display='none';
+//     q16.style.display='none';
+//     q17.style.display='none';
+//     q18.style.display='none';
+//     q19.style.display='none';
+//     q20.style.display='none';
+// }
+// num14.onclick=function(){
+//     q1.style.display='none';
+//     q2.style.display='none';
+//     q3.style.display='none';
+//     q4.style.display='none';
+//     q5.style.display='none';
+//     q6.style.display='none';
+//     q7.style.display='none';
+//     q8.style.display='none';
+//     q9.style.display='none';
+//     q10.style.display='none';
+//     q11.style.display='none';
+//     q12.style.display='none';
+//     q13.style.display='none';
+//     q14.style.display='flex';
+//     q15.style.display='none';
+//     q16.style.display='none';
+//     q17.style.display='none';
+//     q18.style.display='none';
+//     q19.style.display='none';
+//     q20.style.display='none';
+// }
+// num15.onclick=function(){
+//     q1.style.display='none';
+//     q2.style.display='none';
+//     q3.style.display='none';
+//     q4.style.display='none';
+//     q5.style.display='none';
+//     q6.style.display='none';
+//     q7.style.display='none';
+//     q8.style.display='none';
+//     q9.style.display='none';
+//     q10.style.display='none';
+//     q11.style.display='none';
+//     q12.style.display='none';
+//     q13.style.display='none';
+//     q14.style.display='none';
+//     q15.style.display='flex';
+//     q16.style.display='none';
+//     q17.style.display='none';
+//     q18.style.display='none';
+//     q19.style.display='none';
+//     q20.style.display='none';
+// }
+// num16.onclick=function(){
+//     q1.style.display='none';
+//     q2.style.display='none';
+//     q3.style.display='none';
+//     q4.style.display='none';
+//     q5.style.display='none';
+//     q6.style.display='none';
+//     q7.style.display='none';
+//     q8.style.display='none';
+//     q9.style.display='none';
+//     q10.style.display='none';
+//     q11.style.display='none';
+//     q12.style.display='none';
+//     q13.style.display='none';
+//     q14.style.display='none';
+//     q15.style.display='none';
+//     q16.style.display='flex';
+//     q17.style.display='none';
+//     q18.style.display='none';
+//     q19.style.display='none';
+//     q20.style.display='none';
+// }
+// num17.onclick=function(){
+//     q1.style.display='none';
+//     q2.style.display='none';
+//     q3.style.display='none';
+//     q4.style.display='none';
+//     q5.style.display='none';
+//     q6.style.display='none';
+//     q7.style.display='none';
+//     q8.style.display='none';
+//     q9.style.display='none';
+//     q10.style.display='none';
+//     q11.style.display='none';
+//     q12.style.display='none';
+//     q13.style.display='none';
+//     q14.style.display='none';
+//     q15.style.display='none';
+//     q16.style.display='none';
+//     q17.style.display='flex';
+//     q18.style.display='none';
+//     q19.style.display='none';
+//     q20.style.display='none';
+// }
+// num18.onclick=function(){
+//     q1.style.display='none';
+//     q2.style.display='none';
+//     q3.style.display='none';
+//     q4.style.display='none';
+//     q5.style.display='none';
+//     q6.style.display='none';
+//     q7.style.display='none';
+//     q8.style.display='none';
+//     q9.style.display='none';
+//     q10.style.display='none';
+//     q11.style.display='none';
+//     q12.style.display='none';
+//     q13.style.display='none';
+//     q14.style.display='none';
+//     q15.style.display='none';
+//     q16.style.display='none';
+//     q17.style.display='none';
+//     q18.style.display='flex';
+//     q19.style.display='none';
+//     q20.style.display='none';
+// }
+// num19.onclick=function(){
+//     q1.style.display='none';
+//     q2.style.display='none';
+//     q3.style.display='none';
+//     q4.style.display='none';
+//     q5.style.display='none';
+//     q6.style.display='none';
+//     q7.style.display='none';
+//     q8.style.display='none';
+//     q9.style.display='none';
+//     q10.style.display='none';
+//     q11.style.display='none';
+//     q12.style.display='none';
+//     q13.style.display='none';
+//     q14.style.display='none';
+//     q15.style.display='none';
+//     q16.style.display='none';
+//     q17.style.display='none';
+//     q18.style.display='none';
+//     q19.style.display='flex';
+//     q20.style.display='none';
+// }
+// num20.onclick=function(){
+//     q1.style.display='none';
+//     q2.style.display='none';
+//     q3.style.display='none';
+//     q4.style.display='none';
+//     q5.style.display='none';
+//     q6.style.display='none';
+//     q7.style.display='none';
+//     q8.style.display='none';
+//     q9.style.display='none';
+//     q10.style.display='none';
+//     q11.style.display='none';
+//     q12.style.display='none';
+//     q13.style.display='none';
+//     q14.style.display='none';
+//     q15.style.display='none';
+//     q16.style.display='none';
+//     q17.style.display='none';
+//     q18.style.display='none';
+//     q19.style.display='none';
+//     q20.style.display='flex';
+// }
