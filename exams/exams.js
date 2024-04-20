9c3bb8716683b448bf3e046349131ca945e7ae53
KAR00M <161459621+KAR00M@users.noreply.github.com>
2024-04-20 14:08:25 +0200

    Update exams.js

---
+++ exams/exams.js
@@ -0,0 +1,57 @@
+// let web_2021_begin=document.getElementById('web_2021_begin');
+// let web_2021=document.getElementById('web_2021')
+// let web_2021_info=document.getElementById('web_2021_info');
+
+
+
+// web_2021_begin.onclick=function(){
+//     web_2021.style.display='flex';
+//     web_2021_info.style.display='none';
+// }
+
+
+
+// let on=document.getElementById('on');
+// let off=document.getElementById('off');
+// let mode=document.getElementById('mode');
+// let nav_b= document.getElementById('nav_b');
+// let nav_h= document.getElementById('nav_h');
+// let web_content=document.getElementById('web_content');
+
+// let web_2021_button=document.getElementsByClassName('.web_2021_button');
+
+
+// on.onclick=function(){
+//     on.style.display='none';
+//     off.style.display='flex';
+//     on.style.color='#b0b3b8';
+//     mode.style.backgroundColor='#b0b3b8';
+//     off.style.backgroundColor='#24313d';
+//     off.style.color='#b0b3b8';
+//     mode.style.backgroundColor='#24313d';
+
+//     nav_b.style.backgroundColor='#b7bfc7';
+//     nav_h.style.color='black';
+
+//     web_2021_button.style.backgroundColor='#8899a6';
+//     web_content.style.backgroundColor='#b0b3b8';
+
+
+    
+// }
+// off.onclick=function(){
+//     on.style.display='flex';
+//     off.style.display='none';
+//     on.style.marginLeft='24px';
+//     on.style.color='#24313d';
+//     mode.style.backgroundColor='#24313d';
+
+//     nav_b.style.backgroundColor='#15202b';
+//     nav_h.style.color='white';
+
+//     web_content.style.backgroundColor='#15202b';
+    
+//     web_2021_button.style.backgroundColor='#24313d';
+    
+  
+// }
